6aad67d73e63d1e31e620b033667e3d57b3fe8f1
resszermate <materesszer2@gmail.com>
2025-05-04 23:15:20 +0200

    chore entities BlogPost table setup skeleton in EF

--- backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5ff4ef3354c7a0f64bd5322abdd6a2e168a02d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d28a516f9d6658b2c6d391cd97f27f8e833a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
