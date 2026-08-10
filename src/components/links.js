423f48aaaf855e9bd534b203a01408cdcfaccead
lily <resszerlili@gmail.com>
2026-08-10 22:37:48 +0200

    refactor: link script event delegation

--- src/components/links.js
+++ src/components/links.js
@@ -1,22 +1,17 @@
-import { navigate } from "astro:transitions/client";
+import { EMAIL } from "../constants/links";
 
-
-const clipboardAlert = document.getElementById('clipboard-alert');
-const emailButton = document.getElementById('email');
-console.log(emailButton)
-emailButton.addEventListener('click', () => {
-    console.log("clibpoardlick")
-    navigator.clipboard.writeText("resszerlili@gmail.com")
-    clipboardAlert.style.display = "block"
-    setTimeout(() => {
-        clipboardAlert.style.display = "none"
-        }, 2000)
-    })
-const linkOpeners = document.getElementsByClassName('linkopener');
-for (const item of linkOpeners){
-    item.addEventListener('click', () => {
-        if (item.ariaLabel){
-            navigate(item.ariaLabel);
-        }
-    })
-}
+document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('click', (event) => {
+    if (event.target.ariaLabel === EMAIL){
+        const clipboardAlert = document.getElementById('clipboard-alert')
+        navigator.clipboard.writeText("resszerlili@gmail.com")
+        clipboardAlert.style.display = "block"
+        setTimeout(() => {
+            clipboardAlert.style.display = "none"
+            }, 2000)
+    }
+    else {
+        open(event.target.ariaLabel)
+    }
+    });
+})
