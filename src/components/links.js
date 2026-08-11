17ba292e67c14c75a6c7b9ec6c3dd6db67ef3c2b
lily <resszerlili@gmail.com>
2026-08-11 20:18:12 +0200

    feat: cancel existing timeout if copy to clipboard clicked again

--- src/components/links.js
+++ src/components/links.js
@@ -1,14 +1,20 @@
 import { EMAIL, GITHUB, LINKEDIN } from "../constants/links";
 
-document.addEventListener('click', async (event) => {
+const links = document.getElementById('links');
+let timeoutID;
+
+links.addEventListener('click', async (event) => {
     if (!event){
         return
     }
     if (event.target.ariaLabel === EMAIL){
+        if (timeoutID){
+            clearTimeout(timeoutID)
+        }
         const clipboardAlert = document.getElementById('clipboard-alert')
         await navigator.clipboard.writeText("resszerlili@gmail.com")
         clipboardAlert.style.display = "block"
-        setTimeout(() => {
+        timeoutID = setTimeout(() => {
             clipboardAlert.style.display = "none"
             }, 2000)
     }
